6319be5461f4cac01616b785fcfcc9a2ea7580dd
DeveloperNatan <natandesouza403@gmail.com>
2026-05-31 15:50:34 -0300

    first

---
+++ server.js
@@ -0,0 +1,73 @@
+require("dotenv").config();
+const express = require("express");
+const nodemailer = require("nodemailer");
+const fs = require("fs");
+const path = require("path");
+const { PDFDocument, StandardFonts, rgb } = require("pdf-lib");
+
+const app = express();
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+async function gerarPdf(nome) {
+  const pdfPath = path.join(__dirname, "certificado.pdf");
+  const pdfBytes = fs.readFileSync(pdfPath);
+
+  const pdfDoc = await PDFDocument.load(pdfBytes);
+  const page = pdfDoc.getPages()[0];
+  const font = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+  const { width } = page.getSize();
+
+  const fontSize = 18;
+  const textWidth = font.widthOfTextAtSize(nome, fontSize);
+  const x = 285;
+  const y = 365;
+
+  page.drawText(nome, {
+    x,
+    y,
+    size: fontSize,
+    font,
+    color: rgb(0, 0, 0),
+  });
+
+  return Buffer.from(await pdfDoc.save());
+}
+
+app.post("/enviar", async (req, res) => {
+  try {
+    const { nome, email } = req.body;
+    const pdfBuffer = await gerarPdf(nome);
+
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'Seu certificado "Meu primeiro código" chegou🎉',
+      html: `<p>Olá ${nome},\n\nSegue em anexo o seu certificado.\n\nParabéns pela conclusão e obrigado pela participação!\n\nAtenciosamente,\nTurma A - Engenharia de Sofwatre </p>`,
+      attachments: [
+        {
+          filename: "certificado.pdf",
+          content: pdfBuffer,
+          contentType: "application/pdf",
+        },
+      ],
+    });
+
+    res.json({ ok: true, message: "Enviado com sucesso" });
+  } catch (err) {
+    res.status(500).json({ ok: false, error: err.message });
+  }
+});
+
+app.listen(3000);
